07ae3a1d91eeddd0c91aa95bc403d9014fef1416
marksjham <marksjham@gmail.com>
2018-02-08 00:18:29 +0000

    need to fix form validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  validates :transaction_date, :description, :amount, :category, presence: true
+  validates :amount, numericality: {greater_than_or_equal_to: 0}
 end
